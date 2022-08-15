e537ad5653356542487b4a669d168efb3ce55515
Deb <deborahdev.io@gmail.com>
2022-08-15 11:50:11 +0200

    set up hash and firewall

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
 use App\Entity\Recipe;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -16,16 +17,16 @@ class AppFixtures extends Fixture
      * */
     private Generator $faker;
 
+
     public function __construct(){
         $this->faker = Factory::create('fr_FR');
     }
-
 
     public function load(ObjectManager $manager): void
     {
         // ingredients
         $ingredients = [];
-        for ($i = 0; $i < 50; $i++){
+        for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(0, 100));
@@ -35,24 +36,38 @@ class AppFixtures extends Fixture
         }
 
         // recipes
-        for ($i = 0; $i < 25; $i++){
+        for ($i = 0; $i < 25; $i++) {
             $recipe = new Recipe();
             $recipe->setName($this->faker->word())
                 ->setPrice(mt_rand(0, 100))
-                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440):null)
-                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50):null)
-                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5):null)
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
                 ->setDescription($this->faker->word(300))
-                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000):null)
+                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
                 ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
 
-            for ($k = 0; $k < mt_rand(5, 15); $k++){
-                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients)-1)]);
+            for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
             }
 
             $manager->persist($recipe);
         }
 
+        // Users
+        for ($i = 0; $i < 10; $i++) {
+
+            $user = new User();
+
+
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) == 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
