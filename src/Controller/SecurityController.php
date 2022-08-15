a6e4b73c1b1c6145428e0d86a318298eeb3c15ee
Deb <deborahdev.io@gmail.com>
2022-08-15 12:51:41 +0200

    add subscription form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -21,5 +25,32 @@ class SecurityController extends AbstractController
     #[Route('/deconnexion', 'security.logout', methods: ['GET'])]
     public function logout(): Void{
         // Nothing to do here ...
+    }
+
+    #[Route('/inscription', 'security.registration', methods: ['GET', 'POST'])]
+    public function registration(Request $request, EntityManagerInterface $manager): Response {
+
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            $this->addFlash(
+                'success',
+                'Votre compte à bien été crée'
+            );
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return  $this->redirectToRoute('security.login');
+        }
+
+        return $this->render('pages/security/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
